771b792dd030bd713727bdf2c32ab4ddcfb1617c
K <skwyha@yahoo.com>
2022-07-12 16:08:04 +0800

    改名并删除远程分支

---
+++ src/com/skwyha/object_oriented/InterfaceCaseDemo01.java
@@ -0,0 +1,22 @@
+package com.skwyha.object_oriented;
+
+// 定义接口A
+interface A1 {
+
+    // 定义抽象方法print()
+    void print();
+}
+
+// 子类实现接口
+class B1 implements A1 {
+    public void print() {
+        System.out.println("接口是为了解决Java不能多继承的问题");
+    }
+}
+
+public class InterfaceCaseDemo01 {
+    public static void main(String[] args) {
+        A1 a = new B1();
+        a.print();
+    }
+}
